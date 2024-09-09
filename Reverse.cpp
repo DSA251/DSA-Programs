b23812882352fa8d45b8995ba1009b0f46e5703f
DSA251 <vaishnavi.bhandaricse2022@indoreinstitute.com>
2024-09-09 11:05:56 +0530

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,22 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main() 
+int main()
 {
-    int n=7;
-    int a[100];
-    for(int i = 0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    int b[100];
-    int k=0;
-    for(int j=n-1;j>=0;j--)
-    {
-        b[k++]=a[j];
-    }
-    for(int i=0;i<n;i++)
-    {
-        cout << b[i] << " ";
-    }
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n/2;i++) swap(a[i],a[n-i-1]);
+    for(int i=0;i<n;i++) cout << a[i] << " ";
     cout << endl;
 }
